d854f78cb3d56fc50734d38d217c160b02586c73
mcnels <smcnels@me.com>
2021-04-03 21:37:54 -0400

    fixed navbar and mobile schedule

--- js/modules/primary-nav.js
+++ js/modules/primary-nav.js
@@ -1,37 +1,34 @@
-export default function PrimaryNav() {
+// cache dom elements
+var body = document.body,
+    navTrigger = document.querySelector(".js-nav-trigger"),
+    container = document.querySelector(".container"),
+    primaryNav = document.querySelector(".js-primary-nav"),
+    primaryNavLinks = document.querySelectorAll(".js-primary-nav a"); // Flag that JS has loaded
 
-    // cache dom elements
-    var body = document.body,
-        navTrigger = document.querySelector(".js-nav-trigger"),
-        container = document.querySelector(".container"),
-        primaryNav = document.querySelector(".js-primary-nav"),
-        primaryNavLinks = document.querySelectorAll(".js-primary-nav a");
+body.classList.remove("no-js");
+body.classList.add("js"); // Hamburger menu
 
-    // Flag that JS has loaded
-    body.classList.remove("no-js");
-    body.classList.add("js");
+navTrigger.addEventListener("click", function () {
+// toggle active class on the nav trigger
+this.classList.toggle("open"); // toggle the active class on site container
 
-    // Hamburger menu
-    navTrigger.addEventListener("click", function(){
+container.classList.toggle("js-nav-active");
+}); // In-menu link click
+
+for (var i = 0; i < primaryNavLinks.length; i++) {
+    var primaryNavLink = primaryNavLinks[i];
+
+    primaryNavLink.onclick = function () {
         // toggle active class on the nav trigger
-        this.classList.toggle("open");
-        // toggle the active class on site container
+        navTrigger.classList.toggle("open"); // immediately hide the nav
+
+        primaryNav.style.opacity = "0"; // once drawer has had time to pull up, restore opacity
+
+        setTimeout(function () {
+        primaryNav.style.opacity = "1";
+        }, 1000); // toggle the active class on site container
+
         container.classList.toggle("js-nav-active");
-    });
-
-    // In-menu link click
-    for(var i=0; i < primaryNavLinks.length; i++){
-        var primaryNavLink = primaryNavLinks[i];
-        primaryNavLink.onclick = function(){
-            // toggle active class on the nav trigger
-            navTrigger.classList.toggle("open");
-            // immediately hide the nav
-            primaryNav.style.opacity= "0";
-            // once drawer has had time to pull up, restore opacity
-            setTimeout(function() { primaryNav.style.opacity= "1"; }, 1000);
-            // toggle the active class on site container
-            container.classList.toggle("js-nav-active");
-        };
-    }
-
+    };
 }
+
--- js/modules/timeline-loading.js
+++ js/modules/timeline-loading.js
@@ -1,16 +1,10 @@
-export default function TimelineLoading() {
-
 var timelineBlocks = document.querySelectorAll(".cd-timeline-block");
-
-  Array.prototype.forEach.call(timelineBlocks, function(el, i){
-
-    var waypoint = new Waypoint({
-      element: el,
-      handler: function() {
-        el.classList.add('fadeInUp');
-      },
-      offset: '75%'
-    })
-
+Array.prototype.forEach.call(timelineBlocks, function (el, i) {
+  var waypoint = new Waypoint({
+    element: el,
+    handler: function handler() {
+      el.classList.add('fadeInUp');
+    },
+    offset: '75%'
   });
-};
+});
