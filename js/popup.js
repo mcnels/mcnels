1836c2da9f2a3beb91218c650d39c633b2c4eda8
mcnels <smcnels@me.com>
2021-09-30 01:40:16 -0400

    Modified rsvp form to be more user friendly

--- js/popup.js
+++ js/popup.js
@@ -1,50 +1,10 @@
 // Developer Info - Arjun (https://github.com/arjun0108)
 var guests = [""];
 
-$(function(){
-    $('#rsvp').on('click', function (event) {
-        showModal();
-		popGuests();
-		event.stopPropagation(); 
-    });
-	$('#modalClose').on('click', function () {
-        hideModal();
-    });
-	$('#closeButton').on('click', function () {
-        hideModal();
-    });
-    
-	// Do nothing when clicking on the modal content
-	$('.modal-content').on('click', function (event) {
-        event.stopPropagation();
-    });
-});
-
-function showModal(){
-	$('#myModal').fadeIn('slow');
-		(function fun(){
-			$('.modal-content').css({'transform':'translateY(-50px)'});
-		})();
-}
-
-function hideModal(){
-	$('#myModal').fadeOut('fast');
-		(function fun2(){
-			$('.modal-content').css({ 'transform':'translateY(0px)' });
-		})();
-}
-
-
-
-$(document).on("click", function () {
-    //click outside of ".nav__dropdown" class itself and menu will be hidden
-	hideModal();
-});
-
 // save info in firebase
 var db = firebase.firestore();
 
-function popGuests() {
+window.onload = function popGuests() {
 	db.collection("afterparty").get()
 	.then(querySnapshot => {
 		querySnapshot.forEach((doc) => {
@@ -60,34 +20,36 @@ function storeData() {
 	
 	document.getElementById("submit").disabled = true;
 
-	// Make namefield required
+	var inputName = document.getElementById("namefield").value;
+	var inputMsg = document.getElementById("msgfield").value;
 
-  var inputName = document.getElementById("namefield").value;
-  var inputMsg = document.getElementById("msgfield").value;
-
-  if (inputName == "") {
-    alert("Name must be filled out");
-	document.getElementById("submit").disabled = false;
-    return false;
-  }
+	if (inputName == "") {
+		alert("Please enter your full name before submitting.");
+		document.getElementById("submit").disabled = false;
+		return false;
+	}
  
-     db.collection("attendees").doc(inputName).set({
-         Message: inputMsg
-     })
-     .then(function() {
-		 hideModal();
-		 document.getElementById("namefield").value = '';
-		 document.getElementById("msgfield").value = '';
+	db.collection("attendees").doc(inputName).set({
+		Message: inputMsg
+	})
+	.then(function() {
+	//  hideModal();
+		document.getElementById("namefield").value = '';
+		document.getElementById("msgfield").value = '';
 
 		if (guests.includes(inputName)){
 			window.location.replace("afterparty.html");
+		} else {
+			document.getElementById('rsvpMsg').style.display = 'none';
+			document.getElementById('entryForm').style.display = 'none';
+			$('#thanks').toggle();
 		}
-         console.log("Doc successful");
-     })
-     .catch(function(error) {
-        console.error("Error writing doc", error);
-     });
 
-	 document.getElementById("submit").disabled= false;
+	})
+	.catch(function(error) {
+	console.error("Error writing doc", error);
+	});
+
+	document.getElementById("submit").disabled= false;
 		
 }
