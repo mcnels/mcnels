b1a6c373e7055ce25b674d81cee7db9de6fa09a8
mcn <smcnels@me.com>
2022-01-08 23:38:29 -0500

    gallery changes

--- js/swipe.js
+++ js/swipe.js
@@ -1,3 +1,6 @@
+// import PhotoSwipe from './libs/photoswipe';
+// import PhotoSwipeUI_Default from './libs/photoswipe-ui-default';
+
 // Photoswipe
 var initPhotoSwipeFromDOM = function initPhotoSwipeFromDOM(gallerySelector) {
   var parseThumbnailElements = function parseThumbnailElements(el) {
@@ -194,7 +197,7 @@ var initPhotoSwipeFromDOM = function initPhotoSwipeFromDOM(gallerySelector) {
     } // Pass data to PhotoSwipe and initialize it
 
 
-    gallery = new _photoswipe.default(pswpElement, _photoswipeUiDefault.default, items, options); // see: http://photoswipe.com/documentation/responsive-images.html
+    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default.default, items, options); // see: http://photoswipe.com/documentation/responsive-images.html
 
     var realViewportWidth,
         useLargeImages = false,
